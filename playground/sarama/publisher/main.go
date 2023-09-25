f3eef17faa4eff4bcc4bbf662b97abd0ec0c713d
dang.nh1 <dang.nh1@teko.vn>
2023-09-25 22:37:13 +0700

    "Runnable kafka publisher"

--- playground/sarama/publisher/main.go
+++ playground/sarama/publisher/main.go
@@ -1,1 +1,62 @@
-package publisher
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/IBM/sarama"
+)
+
+func newSyncPublisher() (sarama.SyncProducer, error) {
+	cfg := sarama.NewConfig()
+
+	cfg.Producer.RequiredAcks = sarama.WaitForAll
+	cfg.Version = sarama.V2_1_0_0
+	cfg.Net.MaxOpenRequests = 1
+
+	cfg.Producer.Compression = sarama.CompressionLZ4
+	cfg.Producer.Idempotent = true
+	cfg.Producer.Return.Successes = true
+
+	cfg.Consumer.Group.Rebalance.GroupStrategies = []sarama.BalanceStrategy{sarama.NewBalanceStrategySticky()}
+
+	brokers := []string{"localhost:9092"}
+
+	return sarama.NewSyncProducer(brokers, cfg)
+}
+
+func main() {
+	sarama.Logger = log.New(os.Stdout, "[sarama] ", log.LstdFlags)
+
+	publisher, err := newSyncPublisher()
+	if err != nil {
+		panic(err)
+	}
+
+	msg := &sarama.ProducerMessage{
+		Topic: "sarama-test",
+		Key:   sarama.ByteEncoder("key"),
+		Value: sarama.ByteEncoder("value 2"),
+		Headers: []sarama.RecordHeader{
+			{
+				Key:   []byte("header-key"),
+				Value: []byte("header-value"),
+			},
+		},
+		Metadata:  nil,
+		Offset:    0,
+		Partition: 0,
+		Timestamp: time.Time{},
+	}
+
+	fmt.Println("Send message")
+	partition, offset, err := publisher.SendMessage(msg)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Value of partition %d , offset %d\n", partition, offset)
+	publisher.Close()
+}
